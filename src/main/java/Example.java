dfca1e1f97b7ca91701c87bd9d3a52c2fd38d0c7
Dean Pullen <dean.pullen@semantico.com>
2012-05-04 14:45:11 +0100

    First draft

---
+++ src/main/java/Example.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: deanpullen
+ * Date: 04/05/2012
+ * Time: 14:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example {
+}
